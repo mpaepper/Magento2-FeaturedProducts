3208cb80597860670c8eac1d738c09801092fc85
Marc Päpper <m.paepper@lemundo.de>
2015-02-09 10:03:39 +0100

    Added setup script for attribute

---
+++ app/code/Lemundo/FeaturedProducts/data/lemundo_featuredproducts_setup/data-install-0.0.1.php
@@ -0,0 +1,33 @@
+<?php
+
+/** @var $installer \Magento\Catalog\Model\Resource\Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->addAttribute(
+    \Magento\Catalog\Model\Product::ENTITY,
+    'lemundo_featured_product',
+    [
+        'type' => 'int',
+        'backend' => '',
+        'frontend' => '',
+        'label' => 'Is Featured Product',
+        'input' => 'select',
+        'class' => '',
+        'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+        'global' => \Magento\Catalog\Model\Resource\Eav\Attribute::SCOPE_GLOBAL,
+        'visible' => true,
+        'required' => false,
+        'user_defined' => true,
+        'default' => '0',
+        'searchable' => false,
+        'filterable' => false,
+        'comparable' => false,
+        'visible_on_front' => false,
+        'used_in_product_listing' => true,
+        'unique' => false
+    ]
+);
+
+$installer->endSetup();
